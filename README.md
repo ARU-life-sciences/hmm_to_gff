bd583d1697d0013b318707c2b5dcca0b399fd8dd
Max Brown <max.carter-brown@aru.ac.uk>
2025-02-19 16:40:44 +0000

    Added support for nhmmscan.
    We can now calculate whether the match from the
    HMM scan was full or partial as well.

--- README.md
+++ README.md
@@ -1,41 +1,11 @@
-# Format the output of hmmscan to a GFF file
+# Various HMM output to GFF
 
-Specifically we are interested in the output from ORFfinder, where the pipeline is:
+Generating GFF3 output from HMM (like) programs. My use case if fairly specific, and I may make more general later on.
 
-```
-fasta -> ORFfinder -> hmmscan -> GFF
-```
-We use the Pfam HMM database to achieve this.
+## Supported formats
 
-Or when we are running cmscan (Infernal):
+- `nhmmscan --tblout` with the `fam` file originating from oatkDB.
+- `cmscan --tblout` with the `cm` file originating from Rfam.
+- `hmmscan --tblout` with the `hmm` file originating from Pfam, and the input fasta from ORFfinder.
 
-```
-fasta -> cmscan -> GFF
-```
-
-Example of metadata which indicates this script will work on the file. No guarantees, no tests (yet).
-
-```
-#
-# Program:         hmmscan
-# Version:         3.4 (Aug 2023)
-# Pipeline mode:   SCAN
-# Query file:      ../data/Acaena_ovalifolia_orfs.fasta
-# Target file:     ../pfam_hmm/Pfam-A.hmm
-# Options:         /software/team301/hmmer-3.4/src/hmmscan --tblout ../data/pfam_hmmscans/Acaena_ovalifolia.tbl --cpu 16 ../pfam_hmm/Pfam-A.hmm ../data/Acaena_ovalifolia_orfs.fasta
-# Current dir:     /lustre/scratch123/tol/teams/blaxter/users/mb39/ARU/mito_structural_variation/annotation/src
-# Date:            Fri Feb  7 19 13 12 2025
-# [ok]
-```
-
-```
-# Program:         cmscan
-# Version:         1.1.4 (Dec 2020)
-# Pipeline mode:   SCAN
-# Query file:      ../data/Acaena_ovalifolia.fasta
-# Target file:     ../rfam_introns/group_II_intron.cm
-# Option settings: /software/team301/infernal-1.1.4-linux-intel-gcc/binaries/cmscan --tblout test.tbl ../rfam_introns/group_II_intron.cm ../data/Acaena_ovalifolia.fasta 
-# Current dir:     /lustre/scratch123/tol/teams/blaxter/users/mb39/ARU/mito_structural_variation/annotation/src
-# Date:            Tue Feb 11 14:09:12 2025
-# [ok]
-```
+If you do not have these specific outputs, do not expect that this will work for you!
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Format the output of hmmscan/cmscan to a GFF file
+// Format the output of hmmscan/cmscan/nhmmscan to a GFF file
 // Specifically we are interested in the output from ORFfinder
 // fasta -> ORFfinder -> hmmscan -> GFF
 // We use the Pfam HMM database to achieve this.
@@ -8,16 +8,116 @@
 // fasta -> cmscan -> GFF
 //
 // We use RFam for this.
+//
+// This is kind of a hacky script which needs to be tidied up to be made more
+// generic than my specific use case.
+
+use std::{collections::HashMap, fmt::Display, str::FromStr};
 
 use calm_io::*;
 use hmm_tblout::Reader;
 
-// program used is ORFfinder
-const SOURCE: [&str; 2] = ["ORFfinder", "cmscan"];
-// feature type is CDS
-const FEATURE: [&str; 2] = ["CDS", "intron"];
+enum Source {
+    Hmmscan,
+    Cmscan,
+    Nhmmscan,
+}
+
+impl Display for Source {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let source = match self {
+            // specific to orffinder
+            Source::Hmmscan => "ORFfinder",
+            Source::Cmscan => "cmscan",
+            // specific to oatkdb output
+            Source::Nhmmscan => "oatkDB",
+        };
+        write!(f, "{}", source)
+    }
+}
+
+impl FromStr for Source {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "hmmscan" => Ok(Source::Hmmscan),
+            "cmscan" => Ok(Source::Cmscan),
+            "nhmmscan" => Ok(Source::Nhmmscan),
+            e => Err(From::from(format!(
+                "Found {}, second arg must be <hmmscan/nhmmscan/cmscan>",
+                e
+            ))),
+        }
+    }
+}
+
+enum Features {
+    CDS,
+    Intron,
+    Gene,
+    Trna,
+    Rrna,
+}
+
+impl Display for Features {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let feature = match self {
+            Features::CDS => "CDS",
+            Features::Intron => "intron",
+            Features::Gene => "gene",
+            Features::Trna => "tRNA",
+            Features::Rrna => "rRNA",
+        };
+        write!(f, "{}", feature)
+    }
+}
+
 // frames are not available
 const FRAME: &str = ".";
+// matches are partial if they are less than 80% the length of the gene/hit
+const COVERAGE: f32 = 0.8;
+
+// now include gene: length map
+const PFAM_DATA: &str = include_str!("../data/Pfam-A.lengths");
+const RFAM_INTRONS: &str = include_str!("../data/introns.length");
+const VIRIDIPLANTAE_DATA: &str = include_str!("../data/viridiplantae_mito_v20250217.lengths");
+
+fn pfam_lengths() -> HashMap<&'static str, usize> {
+    PFAM_DATA
+        .lines()
+        .filter_map(|line| {
+            let mut parts = line.split_whitespace();
+            let gene = parts.next()?;
+            let length = parts.next()?.parse::<usize>().ok()? * 3; // *3 to account for aa -> nt. Note this isn't perfect (frame shifts, start/stop codons/non coding regions...)
+            Some((gene, length))
+        })
+        .collect()
+}
+
+fn rfam_lengths() -> HashMap<&'static str, usize> {
+    RFAM_INTRONS
+        .lines()
+        .filter_map(|line| {
+            let mut parts = line.split_whitespace();
+            let gene = parts.next()?;
+            let length = parts.next()?.parse::<usize>().ok()?;
+            Some((gene, length))
+        })
+        .collect()
+}
+
+fn viridiplantae_lengths() -> HashMap<&'static str, usize> {
+    VIRIDIPLANTAE_DATA
+        .lines()
+        .filter_map(|line| {
+            let mut parts = line.split_whitespace();
+            let gene = parts.next()?;
+            let length = parts.next()?.parse::<usize>().ok()?;
+            Some((gene, length))
+        })
+        .collect()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // load in the command line arguments
@@ -25,36 +125,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // we only expect one arg, the path to the HMM file
     if args.len() != 3 {
         let _ = stderrln!("hmm_to_gff by Max Brown");
-        let _ = stderrln!("Usage: {} <HMM tblout file> <hmmscan/cmscan>", args[0]);
+        let _ = stderrln!(
+            "Usage: {} <HMM tblout file> <hmmscan/cmscan/nhmmscan>",
+            args[0]
+        );
+        let _ = stderrln!("Note hmmscan output is specific for ORFfinder output.");
         std::process::exit(1);
     }
 
     let file_path = &args[1];
-    let hmmer_type = &args[2];
-
-    if hmmer_type != "hmmscan" && hmmer_type != "cmscan" {
-        let _ = stderrln!("Usage: {} <HMM tblout file> <**hmmscan/cmscan**>", args[0]);
-        std::process::exit(1);
-    }
+    let source = Source::from_str(&args[2])?;
 
     // now create a HMMER tblout reader
     let mut reader = Reader::from_path(file_path)?;
 
     let mut gff_records = Vec::new();
 
-    match hmmer_type.as_str() {
-        "hmmscan" => {
+    match source {
+        Source::Hmmscan => {
             // hmmscan output
             process_hmmscan_from_orffinder(&mut reader, &mut gff_records)?;
         }
-        "cmscan" => {
+        Source::Cmscan => {
             // cmscan output
             process_cmscan_from_infernal(&mut reader, &mut gff_records)?;
         }
-        _ => {
-            // this should never happen
-            let _ = stderrln!("Invalid HMMER type");
-            std::process::exit(1);
+        Source::Nhmmscan => {
+            process_nhmmscan_from_oatkdb(&mut reader, &mut gff_records)?;
         }
     }
 
@@ -63,8 +160,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn process_hmmscan_from_orffinder<R: std::io::Read>(
     reader: &mut Reader<R>,
-    gff_records: &mut Vec<(String, &str, &str, i32, i32, f32, char, &str, String)>,
+    gff_records: &mut Vec<(String, String, String, i32, i32, f32, char, &str, String)>,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    // get the lengths of each of the genes
+    let pfam_lengths = pfam_lengths();
+
     for record in reader.records() {
         let record = record?;
 
@@ -99,21 +199,34 @@ fn process_hmmscan_from_orffinder<R: std::io::Read>(
             '-'
         };
 
+        // do the length analysis
+        let aligned_length = (final_end - final_start) as f32;
+        let gene_length = *pfam_lengths.get(record.target_name().as_str()).unwrap() as f32;
+
+        // make the coverage a descriptive tag, with % coverage included
+        // if partial
+        let coverage = if aligned_length / gene_length < COVERAGE {
+            format!("partial:{:.2}", (aligned_length / gene_length) * 100.0)
+        } else {
+            "full".to_string()
+        };
+
         // and we make the attribute here in the form:
         // query_name_full=;accession=;target_name=;description=;
         let attr = format!(
-            "query_name={};pfam_accession={};target_name={};description={};e_value={:.2e}",
+            "query_name={};pfam_accession={};target_name={};description={};coverage={};e_value={:.2e}",
             record.query_name(),
             record.target_accession(),
             record.target_name(),
             record.description().replace(" ", "_"),
+            coverage,
             record.e_value_full().unwrap(),
         );
 
         gff_records.push((
             seqname,
-            SOURCE[0],
-            FEATURE[0],
+            Source::Hmmscan.to_string(),
+            Features::CDS.to_string(),
             final_start as i32,
             final_end as i32,
             score,
@@ -155,15 +268,23 @@ fn process_hmmscan_from_orffinder<R: std::io::Read>(
 
 fn process_cmscan_from_infernal<R: std::io::Read>(
     reader: &mut Reader<R>,
-    gff_records: &mut Vec<(String, &str, &str, i32, i32, f32, char, &str, String)>,
+    gff_records: &mut Vec<(String, String, String, i32, i32, f32, char, &str, String)>,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let rfam_intron_lengths = rfam_lengths();
     let mut id = 1;
     for record in reader.records() {
         let record = record?;
 
+        // skip records that don't meet the significance threshold
+        // i.e. below 1e-05
+
+        if record.e_value().unwrap() > 1e-05 {
+            continue;
+        }
+
         let seqid = record.query_name();
-        let source = SOURCE[1];
-        let feature_type = FEATURE[1];
+        let source = Source::Cmscan.to_string();
+        let feature_type = Features::Intron.to_string();
 
         let start = record.seq_from().unwrap();
         let end = record.seq_to().unwrap();
@@ -176,13 +297,28 @@ fn process_cmscan_from_infernal<R: std::io::Read>(
 
         let strand = record.strand().unwrap();
 
+        // do the length analysis
+        let aligned_length = (updated_end - updated_start) as f32;
+        let gene_length = *rfam_intron_lengths
+            .get(record.target_name().as_str())
+            .unwrap() as f32;
+
+        // make the coverage a descriptive tag, with % coverage included
+        // if partial
+        let coverage = if aligned_length / gene_length < COVERAGE {
+            format!("partial:{:.2}", (aligned_length / gene_length) * 100.0)
+        } else {
+            "full".to_string()
+        };
+
         // Rfam accession, description
         let attributes = format!(
-            "ID={}_{};rfam_accession={};description={};e_value={:.2e}",
+            "ID={}_{};rfam_accession={};description={};coverage={};e_value={:.2e}",
             seqid,
             id,
             record.target_accession(),
             record.description().replace(" ", "_"),
+            coverage,
             record.e_value().unwrap()
         );
 
@@ -229,5 +365,113 @@ fn process_cmscan_from_infernal<R: std::io::Read>(
         let _ = stdoutln!("{}", gff_line);
     }
 
+    Ok(())
+}
+
+fn process_nhmmscan_from_oatkdb<R: std::io::Read>(
+    reader: &mut Reader<R>,
+    gff_records: &mut Vec<(String, String, String, i32, i32, f32, char, &str, String)>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let viridiplantae_lengths = viridiplantae_lengths();
+
+    let mut id = 1;
+    for record in reader.records() {
+        let record = record?;
+
+        // skip records that don't meet the significance threshold
+        // i.e. below 1e-05
+
+        if record.e_value().unwrap() > 1e-05 {
+            continue;
+        }
+
+        let seqid = record.query_name();
+        let genid = record.target_name();
+        let source = Source::Nhmmscan.to_string();
+
+        let feature_type = match record.target_name().contains("trn") {
+            true => Features::Trna.to_string(),
+            false => match record.target_name().contains("rrn") {
+                true => Features::Rrna.to_string(),
+                false => Features::Gene.to_string(),
+            },
+        };
+
+        let start = record.env_from().unwrap();
+        let end = record.env_to().unwrap();
+
+        // account for the fact that sometimes the start is larger than the end
+        let updated_start = if start < end { start } else { end };
+        let updated_end = if start < end { end } else { start };
+
+        let score = record.score().unwrap();
+
+        let strand = record.strand().unwrap();
+
+        // do the length analysis
+        let aligned_length = (updated_end - updated_start) as f32;
+        let gene_length = *viridiplantae_lengths
+            .get(record.target_name().as_str())
+            .unwrap() as f32;
+
+        // make the coverage a descriptive tag, with % coverage included
+        // if partial
+        let coverage = if aligned_length / gene_length < COVERAGE {
+            format!("partial:{:.2}", (aligned_length / gene_length) * 100.0)
+        } else {
+            "full".to_string()
+        };
+
+        let attributes = format!(
+            "target_name={};ID={}_{};coverage={};e_value={:.2e}",
+            genid,
+            seqid,
+            id,
+            coverage,
+            record.e_value().unwrap()
+        );
+
+        gff_records.push((
+            seqid,
+            source,
+            feature_type,
+            updated_start,
+            updated_end,
+            score,
+            // hacky af
+            strand.to_string().chars().next().unwrap(),
+            FRAME,
+            attributes,
+        ));
+
+        id += 1;
+    }
+
+    // sort the GFF on sequence name and start position
+    gff_records.sort_by(|a, b| {
+        if a.0 == b.0 {
+            a.3.cmp(&b.3)
+        } else {
+            a.0.cmp(&b.0)
+        }
+    });
+
+    let _ = stdoutln!("##gff-version 3");
+
+    for record in gff_records {
+        let gff_line = format!(
+            "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+            record.0,
+            record.1,
+            record.2,
+            record.3,
+            record.4,
+            record.5,
+            record.6,
+            record.7,
+            record.8
+        );
+        let _ = stdoutln!("{}", gff_line);
+    }
     Ok(())
 }
